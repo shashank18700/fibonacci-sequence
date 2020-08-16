77e8b20b520daa9efc290a0c12167fcd588db801
shashank <65406627+shashank18700@users.noreply.github.com>
2020-08-16 22:24:09 +0530

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -3,7 +3,8 @@ def fibo(n):
     b=1
     a=c
     lst=[0,1,1,2]
-    
+    #it requires further improvement
+    #i am going to add feature which is able to give fibonacci sequence between any no.
     while len(lst)<n:
         a=c
         c=c+b
